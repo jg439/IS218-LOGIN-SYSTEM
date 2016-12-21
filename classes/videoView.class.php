7a91271807c6812d3c23f534ef29d899005b57b4
Julia Garcia <jg439@njit.edu>
2016-12-21 13:51:17 -0500

    adding final video view

--- classes/videoView.class.php
+++ classes/videoView.class.php
@@ -1,7 +1,9 @@
 <?php
   class videoView {
        public function getHTML($errors='') {
-       $video = '<h1>Tutorial Video</h1>';
+       $video = '<h1>Tutorial Video</h1>
+
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/0dVJWqHwhZo" frameborder="0" allowfullscreen></iframe>';
           return $video;
        }
   }
